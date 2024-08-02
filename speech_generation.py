d510c1aa7792438be00c5693921ea2831aa75404
FlammAlpha <flammalpha@outlook.com>
2024-08-02 19:40:32 +0200

    fixed elevenlabs / speech generation

--- speech_generation.py
+++ speech_generation.py
@@ -1,9 +1,9 @@
-from elevenlabs import generate, set_api_key
-from elevenlabs.api import History, User
+from elevenlabs.client import ElevenLabs
 
 class Voice:
     def __init__(self, token: str, name: str = "Glinda") -> None:
-        set_api_key(token)
+        self.__api_key = token
+        self.__client = ElevenLabs(api_key=self.__api_key)
         self.__voice_name = name
 
     def get_voice_bytes(self, prompt: str) -> bytes:
@@ -12,7 +12,7 @@ class Voice:
             print("Fetching audio from ElevenLabs")
             print(
                 f"Using {len(prompt)} characters out of {remaining} remaining.")
-            audio_bytes = generate(prompt, voice=self.__voice_name)
+            audio_bytes = self.__client.generate(prompt, voice=self.__voice_name)
             print(
                 f"Remaining characters: {self.get_character_remaining()}")
             return audio_bytes
@@ -22,14 +22,15 @@ class Voice:
         raise Exception("Unable to generate voice")
 
     def get_voice_bytes_history(self, prompt: str) -> bytes:
-        for historyItem in History.from_api().items:
+        client = ElevenLabs(api_key=self.__api_key)
+        for historyItem in client.history.get_all().items:
             if historyItem.text == prompt:
                 audio_bytes = historyItem.audio
                 return audio_bytes
         raise Exception("Voice file not found")
 
     def remove_history(self, prompt: str) -> None:
-        for historyItem in History.from_api().items:
+        for historyItem in self.__client.history.get_all().items:
             if historyItem.text == prompt:
                 historyItem.delete()
                 print("Successfully deleted voice")
@@ -37,9 +38,9 @@ class Voice:
         raise Exception("Could not find voice")
 
     def get_character_remaining(self) -> int:
-        user = User.from_api()
-        limit = user.subscription.character_limit
-        current = user.subscription.character_count
+        subscription = self.__client.user.get_subscription()
+        limit = subscription.character_limit
+        current = subscription.character_count
         percentage = current / limit * 100
         print(f"Used up {current} out of {limit} characters ({percentage}%).")
         return limit - current
