2e22a9bfeb4c05750b67813f1622e3370e3d5734
= <=>
2025-05-24 21:26:25 +0200

    add temperature to text generation

--- text_generation.py
+++ text_generation.py
@@ -24,20 +24,21 @@ class Chat:
         print(f"Response with {len(response)} characters")
         return response
 
-    async def get_response_async(self, message_history: dict, model_version: str = None) -> str:
+    async def get_response_async(self, message_history: dict, model_version: str = None, temperature: float = None) -> str:
         '''Fetches response from ChatGPT with entire message history'''
         fetch_model_version = model_version if model_version is not None else self.__model_version
 
         print("Fetching response from ChatGPT")
         completion = await self.__async_client.chat.completions.create(
-            model=fetch_model_version, messages=message_history)
+            model=fetch_model_version, temperature=temperature,
+            messages=message_history)
 
         response = completion.choices[0].message.content
 
         # print(response)
         print(f"Response with {len(response)} characters")
         return response
-    
+
     def get_model_list(self) -> List[str]:
         model_list = self.__client.models.list()._get_page_items()
         parsed_model_list: List[str] = list()
