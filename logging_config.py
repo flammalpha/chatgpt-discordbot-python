3eba20896d50931ac686eaaea344eb20b4ae4448
= <=>
2025-06-09 17:35:06 +0200

    improved logger config

--- logging_config.py
+++ logging_config.py
@@ -1,29 +1,33 @@
 import logging
 from logging.handlers import TimedRotatingFileHandler
+from pathlib import Path
 
 
-def setup_logger(log_file: str = "bot.py", log_level = logging.DEBUG):
+def setup_logger(log_file: str = "logs/bot.log", log_level=logging.DEBUG):
     logger = logging.getLogger()
     logger.setLevel(level=log_level)
 
     if not logger.handlers:
-        stream_logger = logging.StreamHandler()
-        stream_logger.setLevel(logging.INFO)
-        stream_logger.setFormatter('%(asctime)s [%(levelname)s] %(message)s')
+        # stream_logger = logging.StreamHandler()
+        # stream_logger.setLevel(logging.INFO)
+        # stream_logger.setFormatter(logging.Formatter(
+        #     '%(asctime)s [%(levelname)s] %(message)s'))
 
+        Path(log_file).parent.mkdir(parents=True, exist_ok=True)
         file_logger = TimedRotatingFileHandler(
-            'logs/bot.log',
+            filename=log_file,
             when="midnight",
             interval=1,
-            backupCount=50 # keep 50 files
+            backupCount=50  # keep 50 files
         )
         file_logger.suffix = "%Y-%m-%d"
-        file_logger.setLevel(logging.DEBUG)
+        file_logger.setLevel(log_level)
         file_logger.setFormatter(logging.Formatter(
             '%(asctime)s [%(levelname)s] %(name)s: %(message)s'))
 
-        logger.addHandler(stream_logger)
+        # logger.addHandler(stream_logger)
         logger.addHandler(file_logger)
+
 
 if __name__ == "__main__":
     setup_logger()
